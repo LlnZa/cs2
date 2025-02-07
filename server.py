2b288a03044fab277f2e734785a473ab67d37add
LlnZa <112642007+LlnZa@users.noreply.github.com>
2025-02-07 11:51:13 +0300

    Update server.py

--- server.py
+++ server.py
@@ -25,5 +25,7 @@ async def read_root():
     return HTMLResponse(content=HTML_CONTENT, status_code=200)
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+port = int(os.getenv("PORT", 8000))
+uvicorn.run(app, host="0.0.0.0", port=port)
+
