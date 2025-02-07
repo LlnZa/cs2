c29a7dd9d3ca3d76b8fcc6940d2058d8ea25b04e
LlnZa <112642007+LlnZa@users.noreply.github.com>
2025-02-07 11:43:20 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+HTML_CONTENT = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CS2 Esports WebApp</title>
+</head>
+<body>
+    <h1>Список ближайших матчей</h1>
+    <ul>
+        <li>G2 vs NAVI - 10:00 12.02.2025</li>
+        <li>FaZe vs Vitality - 14:00 12.02.2025</li>
+        <li>ENCE vs Heroic - 18:00 12.02.2025</li>
+    </ul>
+</body>
+</html>
+"""
+
+@app.get("/")
+async def read_root():
+    return HTMLResponse(content=HTML_CONTENT, status_code=200)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
